38d76baaea0486077acd6b57f52e25169ecd4f2e
shin jiwon <103318297+ena-isme@users.noreply.github.com>
2023-11-18 14:35:04 +0900

    Initial commit

---
+++ Millie_iOS/Millie_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Millie_iOS
+//
+//  Created by 신지원 on 11/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
