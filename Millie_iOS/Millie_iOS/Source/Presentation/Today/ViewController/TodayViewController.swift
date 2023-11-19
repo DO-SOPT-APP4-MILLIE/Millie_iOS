111a7eae7bdce5fbdcce521f16d46f7869adf82f
shinzz410 <tlswldnjs410@gmail.com>
2023-11-20 03:33:09 +0900

    test

--- Millie_iOS/Millie_iOS/Source/Presentation/Today/ViewController/TodayViewController.swift
+++ Millie_iOS/Millie_iOS/Source/Presentation/Today/ViewController/TodayViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class TodayViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+//        target()
+//        register()
+//        
+//        setStyle()
+//        setHieararchy()
+//        setLayout()
     }
-
-
+    
+    
 }
 
