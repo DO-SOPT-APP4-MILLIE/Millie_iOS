0d127e18a2fe98fcad9e76a3c5123ddb330925c3
Sangwoo Byeon <tkddn0518@naver.com>
2023-11-21 22:18:43 +0900

    Feat(#5): 전체 수직 ScrollView 추가

--- Millie_iOS/Millie_iOS/Source/Presentation/Detail/ViewController/DetailViewController.swift
+++ Millie_iOS/Millie_iOS/Source/Presentation/Detail/ViewController/DetailViewController.swift
@@ -9,10 +9,20 @@ import UIKit
 import SnapKit
 
 class DetailViewController: UIViewController {
+    
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.showsVerticalScrollIndicator = false
+        return scrollView
+    }()
+    
+    private let contentView: UIView = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupNavigtion()
+        setupLayout()
     }
     
     private func setupNavigtion() {
@@ -25,6 +35,23 @@ class DetailViewController: UIViewController {
         
         self.navigationItem.rightBarButtonItems = [rightSettingButton, rightStarButton]
         self.navigationController?.navigationBar.tintColor = .black
+    }
+    
+    private func setupLayout() {
+        self.view.backgroundColor = .white
+        self.view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+    
+        scrollView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        contentView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+            $0.width.equalTo(view.snp.width)
+            $0.bottom.equalTo(view.snp.bottom)
+//            $0.height.greaterThanOrEqualTo(view.snp.height).priority(.low)
+        }
     }
     
     @objc
