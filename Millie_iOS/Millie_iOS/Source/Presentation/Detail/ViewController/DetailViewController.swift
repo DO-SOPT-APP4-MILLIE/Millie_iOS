59ca54ec4908c7c172e2dead9b1e259506153cac
Sangwoo Byeon <tkddn0518@naver.com>
2023-11-21 22:15:31 +0900

    Feat(#5): 네비게이션 바 아이템 추가

---
+++ Millie_iOS/Millie_iOS/Source/Presentation/Detail/ViewController/DetailViewController.swift
@@ -0,0 +1,44 @@
+//
+//  DetailViewController.swift
+//  Millie_iOS
+//
+//  Created by 변상우 on 11/20/23.
+//
+
+import UIKit
+import SnapKit
+
+class DetailViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupNavigtion()
+    }
+    
+    private func setupNavigtion() {
+        let leftBackButton = UIBarButtonItem(image: UIImage(named: "ic_arrow_left_line"), style: .plain, target: self, action: #selector(leftBackButtonTap))
+        self.navigationItem.leftBarButtonItem = leftBackButton
+        
+        let rightStarButton = UIBarButtonItem(image: UIImage(named: "ic_star_slim_line"), style: .plain, target: self, action: #selector(rightStarButtonTap))
+        
+        let rightSettingButton = UIBarButtonItem(image: UIImage(named: "ic_menu_kebab"), style: .plain, target: self, action: #selector(rightSettingButtonTap))
+        
+        self.navigationItem.rightBarButtonItems = [rightSettingButton, rightStarButton]
+        self.navigationController?.navigationBar.tintColor = .black
+    }
+    
+    @objc
+    private func leftBackButtonTap() {
+        print("네비게이션 POP")
+    }
+    
+    @objc
+    private func rightStarButtonTap() {
+        
+    }
+    
+    @objc
+    private func rightSettingButtonTap() {
+        
+    }
+}
