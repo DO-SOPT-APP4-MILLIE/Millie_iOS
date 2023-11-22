98cf270b6ad88135d42d7f13b19eff3d222942c5
Sangwoo Byeon <tkddn0518@naver.com>
2023-11-22 13:43:59 +0900

    Fix(#5): Conflict 났던 파일 수정

---
+++ Millie_iOS/Millie_iOS/Resource/Extensions/CALayer+.swift
@@ -0,0 +1,33 @@
+//
+//  CALayer+.swift
+//  Millie_iOS
+//
+//  Created by 변상우 on 11/21/23.
+//
+import UIKit
+
+extension CALayer {
+    func addBorder(_ arr_edge: [UIRectEdge], color: UIColor, width: CGFloat) {
+        for edge in arr_edge {
+            let border = CALayer()
+
+            switch edge {
+            case UIRectEdge.top:
+                border.frame = CGRect(x: 0, y: 0, width: frame.width, height: width)
+                break
+            case UIRectEdge.bottom:
+                border.frame = CGRect(x: 0, y: frame.height - width, width: frame.width, height: width)
+                break
+            case UIRectEdge.left:
+                border.frame = CGRect(x: 0, y: 0, width: width, height: frame.height)
+            case UIRectEdge.right:
+                border.frame = CGRect.init(x: frame.width - width, y: 0, width: width, height: frame.height)
+                break
+            default:
+                break
+            }
+            border.backgroundColor = color.cgColor;
+            self.addSublayer(border)
+        }
+    }
+}
---
+++ Millie_iOS/Millie_iOS/Resource/Extensions/UIStackView+.swift
@@ -0,0 +1,15 @@
+//
+//  UIStackView+.swift
+//  Millie_iOS
+//
+//  Created by 변상우 on 11/21/23.
+//
+import UIKit
+
+extension UIStackView {
+    func addArrangedSubviews(_ views: UIView...) {
+        views.forEach {
+            self.addArrangedSubview($0)
+        }
+    }
+}
