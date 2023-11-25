81459c680c20e06a80d140334b2eb689f4129065
shinzz410 <tlswldnjs410@gmail.com>
2023-11-25 14:19:24 +0900

    Style(#18): 이미지 네이밍 선언

--- Millie_iOS/Millie_iOS/Resource/Constants/Image.swift
+++ Millie_iOS/Millie_iOS/Resource/Constants/Image.swift
@@ -9,29 +9,55 @@ import UIKit
 
 public enum Image {
     
+    //Arrow
     static let arrowLeft = UIImage(named: "ic_arrow_left_line")!
     static let arrowRight = UIImage(named: "ic_arrow_right_line")!
     static let arrowTop = UIImage(named: "ic_arrow_top")!
     static let arrowBottom = UIImage(named: "ic_arrow_bottom")!
     static let arrowDown = UIImage(named: "ic_arrow_down")!
+    static let arrowRight2 = UIImage(named: "ic_arrow2_right")!
+    
+    //Audio
+    static let audioLine = UIImage(named: "ic_audio_line")!
+    static let audioFill = UIImage(named: "ic_audio_fill")!
+    static let audioBook = UIImage(named: "ic_audiobook")!
+    
+    //Book
+    static let bookLine = UIImage(named: "ic_book_line")!
+    static let bookOpen = UIImage(named: "ic_book_open")!
+    static let bookmarkPdf = UIImage(named: "ic_bookmark_pdf")!
+    static let bookmarkLine = UIImage(named: "ic_bookmark_line")!
+    static let bookmarkSummary = UIImage(named: "ic_bookmark_summary")!
+    static let bookmarkHeadphone = UIImage(named: "ic_bookmark_headphone")!
+    
+    //Etc
+    static let check = UIImage(named: "ic_check_line")!
+    static let clap = UIImage(named: "ic_clap_line")!
+    static let pause = UIImage(named: "ic_pause")!
+    static let playRight = UIImage(named: "ic_play_rigth_fill")!
+    static let put = UIImage(named: "ic_put")!
+    static let refresh = UIImage(named: "ic_refresh")!
+    static let sound = UIImage(named: "ic_sound")!
+    static let star = UIImage(named: "ic_star_line")!
     static let heart = UIImage(named: "ic_heart")!
+    static let info = UIImage(named: "icon_info_line")!
+    static let info2 = UIImage(named: "ic_info_2")!
     static let wandok = UIImage(named: "ic_wandok_line")!
     static let feed = UIImage(named: "ic_feed_line")!
     static let grid = UIImage(named: "ic_feed_num_line")!
+    static let add = UIImage(named: "ic_add post_line")!
+    static let hamburger = UIImage(named: "ic_menu_hamburger")!
+    static let profile = UIImage(named: "image_profile")!
+    static let kebab = UIImage(named: "ic_menu_kebab")!
+    static let search = UIImage(named: "ic_search")!
+    static let bell = UIImage(named: "ic_bell_line")!
     
+    //Today
     static let thumbsUp = UIImage(named: "today_ic_thumbs_up")!
     static let smallBookImage = UIImage(named: "today_img_best_01")
     static let todayFistImage = UIImage(named: "today_card_week_1")
     static let todayMonthlyImage = UIImage(named: "today_new_book_01")
     static let todayOriginalImage = UIImage(named: "today_card_original_01")
     static let todayPreferenceImage = UIImage(named: "today_rec_02")
-
-    static let add = UIImage(named: "ic_add post_line")!
-    static let hamburger = UIImage(named: "ic_menu_hamburger")!
-    static let profile = UIImage(named: "image_profile")!
-    static let audioLine = UIImage(named: "ic_audio_line")!
-    static let bookmarkLine = UIImage(named: "ic_bookmark_line")!
-    static let kebab = UIImage(named: "ic_menu_kebab")!
-    static let search = UIImage(named: "ic_search")!
     
 }
