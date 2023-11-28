733ebf88c279fe121ebf004e2cbf525c95adfa5d
shinzz410 <tlswldnjs410@gmail.com>
2023-11-28 14:53:33 +0900

    Feat(#29): 네트워크 세팅

---
+++ Millie_iOS/Millie_iOS/Resource/Constants/Config.swift
@@ -0,0 +1,12 @@
+//
+//  Config.swift
+//  Millie_iOS
+//
+//  Created by 신지원 on 11/28/23.
+//
+
+import Foundation
+
+struct Config {
+    static let baseURL = Bundle.main.infoDictionary?["BASE_URL"] as! String
+}
---
+++ Millie_iOS/Millie_iOS/Resource/Constants/URLs.swift
@@ -0,0 +1,23 @@
+//
+//  URLs.swift
+//  Millie_iOS
+//
+//  Created by 신지원 on 11/28/23.
+//
+
+import Foundation
+
+public enum URLs {
+    
+    //도서 베스트 List
+    static let bestBook = "/api/books"
+    
+    //도서 Detail
+    static let detailBook = "/api/books/{bookId}"
+
+    //유저 도서 담기
+    static let archiveBook = "/api/books/{bookId}/archive"
+    
+    //유저 담은 책 List
+    static let collectionsBook = "/api/me/collections"
+}
